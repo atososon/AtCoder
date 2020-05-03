7a4742d294330c7da4622085b87a5723a6bbd6ff
atososon <atososon165@gmail.com>
2020-05-03 21:02:22 +0900

    [AC] abc166 abc166_a

---
+++ atcoder.jp/abc166/abc166_a/Main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char s[4];
+    scanf("%s",s);
+    if(strcmp(s,"ABC")==0){
+        printf("ARC\n");
+    }else{
+        printf("ABC\n");
+    }
+    return 0;
+}
