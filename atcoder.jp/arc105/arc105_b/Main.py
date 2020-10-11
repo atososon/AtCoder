81a2627ae54a32ce406dfcb59e5561d0f348b8a2
atososon <atososon165@gmail.com>
2020-10-12 01:34:49 +0900

    [AC] arc105 arc105_b

---
+++ atcoder.jp/arc105/arc105_b/Main.py
@@ -0,0 +1,11 @@
+def gcd(a,b):
+    if b==0:
+        return a
+    else:
+        return gcd(b,a%b)
+n = int(input())
+a = list(map(int, input().split()))
+ans = a[0]
+for i in range(n):
+    ans = gcd(ans, a[i])
+print(ans)
